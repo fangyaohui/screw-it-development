03429707b0084a738f0d24d89f89af59cc3ad885
fangyaohui <2944163240@qq.com>
2024-07-28 17:19:18 +0800

    日志更改 & 新建博客模块

---
+++ screw-it-development-platform/screw-it-development-bus/screw-it-development-blog/src/main/java/com/fang/screw/blog/ComFangScrewBlogApplication.java
@@ -0,0 +1,18 @@
+package com.fang.screw.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+//@EnableDubbo
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(value = {"com.fang.screw.communal.*", "com.fang.screw.blog.*"})
+public class ComFangScrewBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComFangScrewBlogApplication.class, args);
+    }
+
+}
