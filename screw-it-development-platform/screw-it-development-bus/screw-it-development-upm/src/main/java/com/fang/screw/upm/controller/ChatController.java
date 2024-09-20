cf861faf240a652c7d493e9f11c09bd6601672ed
fangyaohui <2944163240@qq.com>
2024-09-20 19:32:51 +0800

    websocket 适配

---
+++ screw-it-development-platform/screw-it-development-bus/screw-it-development-upm/src/main/java/com/fang/screw/upm/controller/ChatController.java
@@ -0,0 +1,40 @@
+package com.fang.screw.upm.controller;
+
+import com.fang.screw.domain.po.ChatMessage;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @FileName MessageController
+ * @Description 负责实时通信
+ * @Author yaoHui
+ * @date 2024-09-20
+ **/
+@Controller
+public class ChatController  {
+
+    // 处理客户端发来的聊天消息
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message) {
+        // 返回消息，广播给订阅 "/topic/messages" 的用户
+        return message;
+    }
+
+    // 处理用户加入事件
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/messages")
+    public ChatMessage addUser(ChatMessage message, SimpMessageHeaderAccessor headerAccessor) {
+        // 将用户加入到WebSocket的会话中
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        message.setContent(message.getSender() + " 加入了聊天");
+        return message;
+    }
+
+}
---
+++ screw-it-development-platform/screw-it-development-domain/screw-it-development-biz/src/main/java/com/fang/screw/domain/po/ChatMessage.java
@@ -0,0 +1,39 @@
+package com.fang.screw.domain.po;
+
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    // Getters and setters
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
